f953d6642eca98a75ba44ffea5e0928b509deb44
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2023-05-15 10:01:07 +0800

    Lecture 25 - Adding Assets for Onboarding Screen

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -2,4 +2,9 @@ const String imagePath = 'assets/images';
 
 class ImageAssets {
   static const String splashLogo = '$imagePath/splash_logo.png';
+
+  static const String onBoardingLogo1 = '$imagePath/on_boarding_logo1.svg';
+  static const String onBoardingLogo2 = '$imagePath/on_boarding_logo2.svg';
+  static const String onBoardingLogo3 = '$imagePath/on_boarding_logo3.svg';
+  static const String onBoardingLogo4 = '$imagePath/on_boarding_logo4.svg';
 }
--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,17 @@
 class AppStrings {
   static const String noRouteFound = 'No route found.';
+
+  static const String onBoardingTitle1 = 'SEE THE BEST COURSE #1';
+  static const String onBoardingTitle2 = 'SEE THE BEST COURSE #2';
+  static const String onBoardingTitle3 = 'SEE THE BEST COURSE #3';
+  static const String onBoardingTitle4 = 'SEE THE BEST COURSE #4';
+
+  static const String onBoardingSubtitle1 =
+      'Tutorial App is an awesome flutter application using clean architecture #1';
+  static const String onBoardingSubtitle2 =
+      'Tutorial App is an awesome flutter application using clean architecture #2';
+  static const String onBoardingSubtitle3 =
+      'Tutorial App is an awesome flutter application using clean architecture #3';
+  static const String onBoardingSubtitle4 =
+      'Tutorial App is an awesome flutter application using clean architecture #4';
 }
