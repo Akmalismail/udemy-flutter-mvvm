d7e595cdea8f81192a74f9faeb04de1e8e20ba81
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2023-05-15 12:07:59 +0800

    Lecture 21 - Adding Timer to Splash Screen

--- lib/presentation/on_boarding/on_boarding.dart
+++ lib/presentation/on_boarding/on_boarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_flutter_mvvm/presentation/resources/color_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
@@ -10,6 +11,11 @@ class OnBoardingView extends StatefulWidget {
 class _OnBoardingViewState extends State<OnBoardingView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      body: const Center(
+        child: Text('Welcome to on boarding!'),
+      ),
+    );
   }
 }
