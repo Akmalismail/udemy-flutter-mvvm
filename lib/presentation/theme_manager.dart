1daaf820c41e5c0fc045e5c8589b5adf81f1633d
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2023-05-15 12:07:24 +0800

    Lecture 11 - Adding App Bar Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:udemy_flutter_mvvm/presentation/color_manager.dart';
+import 'package:udemy_flutter_mvvm/presentation/font_manager.dart';
+import 'package:udemy_flutter_mvvm/presentation/styles_manager.dart';
 import 'package:udemy_flutter_mvvm/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors of the app
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1, // for cases like disabled button
-      colorScheme: ColorScheme.fromSeed(
-          seedColor: ColorManager.primary, secondary: ColorManager.grey),
+    // main colors of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1, // for cases like disabled button
+    splashColor: ColorManager.primaryOpacity70, // ripple color
+    colorScheme: ColorScheme.fromSeed(
+        seedColor: ColorManager.primary, secondary: ColorManager.grey),
 
-      // card view theme
-      cardTheme: CardTheme(
-          color: ColorManager.white,
-          shadowColor: ColorManager.grey,
-          elevation: AppSize.s4)
+    // card view theme
+    cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4),
 
-      // app bar theme
+    // app bar theme
+    appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.primaryOpacity70,
+        titleTextStyle:
+            getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16)),
 
-      // button theme
+    // button theme
 
-      // text theme
+    // text theme
 
-      // input decoration theme (text form)
-      );
+    // input decoration theme (text form)
+  );
 }
