b2cb46763998a6275391a7e0abb6d13ee3bd5eed
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2023-05-17 16:17:22 +0800

    Lecture 48 - Domain Layer - Creating Models

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -1,7 +1,30 @@
 class SliderObject {
-  String title;
-  String subTitle;
-  String image;
+  final String title;
+  final String subTitle;
+  final String image;
 
   SliderObject(this.title, this.subTitle, this.image);
+}
+
+class Customer {
+  final String id;
+  final String name;
+  final int numOfNotifications;
+
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contact {
+  final String phone;
+  final String link;
+  final String email;
+
+  Contact(this.phone, this.link, this.email);
+}
+
+class Authentication {
+  Customer customer;
+  Contact contact;
+
+  Authentication(this.customer, this.contact);
 }
