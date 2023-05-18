8238f9105dcef8ef8a9fa4e7c0bf5704d9565065
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2023-05-18 15:31:05 +0800

    Lecture 57 - Data Layer - Adding Responses Codes

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -13,3 +13,23 @@ enum DataSource {
   cacheError,
   noInternetConnection,
 }
+
+class ResponseCode {
+  // http status codes.
+  static const int success = 200;
+  static const int noContent = 201;
+  static const int badRequest = 400;
+  static const int forbidden = 403;
+  static const int unauthorized = 401;
+  static const int notFound = 404;
+  static const int internalServerError = 500;
+
+  // local status codes.
+  static const int unknown = -1;
+  static const int connectTimeout = -2;
+  static const int cancel = -3;
+  static const int receiveTimeout = -4;
+  static const int sendTimeout = -5;
+  static const int cacheError = -6;
+  static const int noInternetConnection = -7;
+}
