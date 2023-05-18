dad0cdf3f7a88ca4c75ef398ef3c8af70d7b5ee4
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2023-05-18 16:37:02 +0800

    Lecture 60 - Data Layer - Adding Error Handler Class

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -1,152 +1,233 @@
+import 'package:dio/dio.dart';
 import 'package:udemy_flutter_mvvm/data/network/failure.dart';
 
 enum HttpStatus {
   success(
-    200,
-    "Success",
+    ResponseCode.success,
+    ResponseMessage.success,
   ),
   noContent(
-    201,
-    'Success with no content.',
+    ResponseCode.noContent,
+    ResponseMessage.noContent,
   ),
   badRequest(
-    400,
-    'Bad request. Please try again later.',
+    ResponseCode.badRequest,
+    ResponseMessage.badRequest,
   ),
   forbidden(
-    403,
-    'Forbidden request.',
+    ResponseCode.forbidden,
+    ResponseMessage.forbidden,
   ),
   unauthorized(
-    401,
-    'User is not authorised.',
+    ResponseCode.unauthorized,
+    ResponseMessage.unauthorized,
   ),
   notFound(
-    404,
-    'URL not found.',
+    ResponseCode.notFound,
+    ResponseMessage.notFound,
   ),
   internalServerError(
-    500,
-    'Something went wrong server side. Please try again later.',
+    ResponseCode.internalServerError,
+    ResponseMessage.internalServerError,
   ),
   unknown(
-    -1,
-    'Something went wrong. Please try again later.',
+    ResponseCode.unknown,
+    ResponseMessage.unknown,
   ),
-  connectTimeout(
-    -2,
-    'Connection timed out.',
+  connectionTimeout(
+    ResponseCode.connectionTimeout,
+    ResponseMessage.connectionTimeout,
   ),
   cancel(
-    -3,
-    'Request was cancelled.',
+    ResponseCode.cancel,
+    ResponseMessage.cancel,
   ),
   receiveTimeout(
-    -4,
-    'Request was timed out.',
+    ResponseCode.receiveTimeout,
+    ResponseMessage.receiveTimeout,
   ),
   sendTimeout(
-    -5,
-    'Request was timed out.',
+    ResponseCode.sendTimeout,
+    ResponseMessage.sendTimeout,
   ),
   cacheError(
-    -6,
-    'Cache error. Please try again later.',
+    ResponseCode.cacheError,
+    ResponseMessage.cacheError,
   ),
   noInternetConnection(
-    -7,
-    'Please check your internet connection.',
+    ResponseCode.noInternetConnection,
+    ResponseMessage.noInternetConnection,
+  ),
+  connectionError(
+    ResponseCode.connectionError,
+    ResponseMessage.connectionError,
+  ),
+  badCertificate(
+    ResponseCode.badCertificate,
+    ResponseMessage.badCertificate,
   );
 
   final int code;
   final String message;
 
   const HttpStatus(this.code, this.message);
-}
 
-enum DataSource {
-  success,
-  noContent,
-  badRequest,
-  forbidden,
-  unauthorized,
-  notFound,
-  internalServerError,
-  connectTimeout,
-  cancel,
-  receiveTimeout,
-  sendTimeout,
-  cacheError,
-  noInternetConnection,
-}
-
-extension DataSourceExtension on DataSource {
-  Failure getFailure() {
-    switch (this) {
-      case DataSource.badRequest:
-        return Failure(
-          ResponseCode.badRequest,
-          ResponseMessage.badRequest,
-        );
-      case DataSource.forbidden:
-        return Failure(
-          ResponseCode.forbidden,
-          ResponseMessage.forbidden,
-        );
-      case DataSource.unauthorized:
-        return Failure(
-          ResponseCode.unauthorized,
-          ResponseMessage.unauthorized,
-        );
-      case DataSource.notFound:
-        return Failure(
-          ResponseCode.notFound,
-          ResponseMessage.notFound,
-        );
-      case DataSource.internalServerError:
-        return Failure(
-          ResponseCode.internalServerError,
-          ResponseMessage.internalServerError,
-        );
-
-      case DataSource.connectTimeout:
-        return Failure(
-          ResponseCode.connectTimeout,
-          ResponseMessage.connectTimeout,
-        );
-      case DataSource.cancel:
-        return Failure(
-          ResponseCode.cancel,
-          ResponseMessage.cancel,
-        );
-      case DataSource.receiveTimeout:
-        return Failure(
-          ResponseCode.receiveTimeout,
-          ResponseMessage.receiveTimeout,
-        );
-      case DataSource.sendTimeout:
-        return Failure(
-          ResponseCode.sendTimeout,
-          ResponseMessage.sendTimeout,
-        );
-      case DataSource.cacheError:
-        return Failure(
-          ResponseCode.cacheError,
-          ResponseMessage.cacheError,
-        );
-      case DataSource.noInternetConnection:
-        return Failure(
-          ResponseCode.noInternetConnection,
-          ResponseMessage.noInternetConnection,
-        );
-      default:
-        return Failure(
-          ResponseCode.unknown,
-          ResponseMessage.unknown,
-        );
-    }
+  Failure get failure {
+    return Failure(
+      code,
+      message,
+    );
   }
 }
+
+// enum DataSource {
+//   success,
+//   noContent,
+//   badRequest,
+//   forbidden,
+//   unauthorized,
+//   notFound,
+//   internalServerError,
+//   unknown,
+//   connectTimeout,
+//   cancel,
+//   receiveTimeout,
+//   sendTimeout,
+//   cacheError,
+//   noInternetConnection,
+// }
+
+class ErrorHandler implements Exception {
+  late Failure failure;
+
+  ErrorHandler.handle(dynamic error) {
+    if (error is DioError) {
+      // dio error from api response
+      failure = _handleError(error);
+    } else {
+      // default error
+      failure = HttpStatus.unknown.failure;
+    }
+  }
+
+  Failure _handleError(DioError error) {
+    HttpStatus status;
+    switch (error.type) {
+      case DioErrorType.connectionTimeout:
+        status = HttpStatus.connectionTimeout;
+        break;
+      case DioErrorType.sendTimeout:
+        status = HttpStatus.sendTimeout;
+        break;
+      case DioErrorType.receiveTimeout:
+        status = HttpStatus.receiveTimeout;
+        break;
+      case DioErrorType.badCertificate:
+        status = HttpStatus.badCertificate;
+        break;
+      case DioErrorType.badResponse:
+        switch (error.response?.statusCode) {
+          case ResponseCode.badRequest:
+            status = HttpStatus.badRequest;
+            break;
+          case ResponseCode.forbidden:
+            status = HttpStatus.forbidden;
+            break;
+          case ResponseCode.unauthorized:
+            status = HttpStatus.unauthorized;
+            break;
+          case ResponseCode.notFound:
+            status = HttpStatus.notFound;
+            break;
+          case ResponseCode.internalServerError:
+            status = HttpStatus.internalServerError;
+            break;
+          default:
+            status = HttpStatus.unknown;
+        }
+        break;
+      case DioErrorType.cancel:
+        status = HttpStatus.cancel;
+        break;
+      case DioErrorType.connectionError:
+        status = HttpStatus.connectionError;
+        break;
+      case DioErrorType.unknown:
+        status = HttpStatus.unknown;
+        break;
+    }
+
+    return status.failure;
+  }
+}
+
+// extension DataSourceExtension on DataSource {
+//   Failure getFailure() {
+//     switch (this) {
+//       case DataSource.badRequest:
+//         return Failure(
+//           ResponseCode.badRequest,
+//           ResponseMessage.badRequest,
+//         );
+//       case DataSource.forbidden:
+//         return Failure(
+//           ResponseCode.forbidden,
+//           ResponseMessage.forbidden,
+//         );
+//       case DataSource.unauthorized:
+//         return Failure(
+//           ResponseCode.unauthorized,
+//           ResponseMessage.unauthorized,
+//         );
+//       case DataSource.notFound:
+//         return Failure(
+//           ResponseCode.notFound,
+//           ResponseMessage.notFound,
+//         );
+//       case DataSource.internalServerError:
+//         return Failure(
+//           ResponseCode.internalServerError,
+//           ResponseMessage.internalServerError,
+//         );
+//       case DataSource.connectTimeout:
+//         return Failure(
+//           ResponseCode.connectionTimeout,
+//           ResponseMessage.connectionTimeout,
+//         );
+//       case DataSource.cancel:
+//         return Failure(
+//           ResponseCode.cancel,
+//           ResponseMessage.cancel,
+//         );
+//       case DataSource.receiveTimeout:
+//         return Failure(
+//           ResponseCode.receiveTimeout,
+//           ResponseMessage.receiveTimeout,
+//         );
+//       case DataSource.sendTimeout:
+//         return Failure(
+//           ResponseCode.sendTimeout,
+//           ResponseMessage.sendTimeout,
+//         );
+//       case DataSource.cacheError:
+//         return Failure(
+//           ResponseCode.cacheError,
+//           ResponseMessage.cacheError,
+//         );
+//       case DataSource.noInternetConnection:
+//         return Failure(
+//           ResponseCode.noInternetConnection,
+//           ResponseMessage.noInternetConnection,
+//         );
+//       case DataSource.unknown:
+//       default:
+//         return Failure(
+//           ResponseCode.unknown,
+//           ResponseMessage.unknown,
+//         );
+//     }
+//   }
+// }
 
 class ResponseCode {
   // http status codes.
@@ -160,12 +241,14 @@ class ResponseCode {
 
   // local status codes.
   static const int unknown = -1;
-  static const int connectTimeout = -2;
+  static const int connectionTimeout = -2;
   static const int cancel = -3;
   static const int receiveTimeout = -4;
   static const int sendTimeout = -5;
   static const int cacheError = -6;
   static const int noInternetConnection = -7;
+  static const int connectionError = -8;
+  static const int badCertificate = -9;
 }
 
 class ResponseMessage {
@@ -181,11 +264,13 @@ class ResponseMessage {
 
   // local status codes.
   static const String unknown = 'Something went wrong. Please try again later.';
-  static const String connectTimeout = 'Connection timed out.';
+  static const String connectionTimeout = 'Connection timed out.';
   static const String cancel = 'Request was cancelled.';
   static const String receiveTimeout = 'Request was timed out.';
   static const String sendTimeout = 'Request was timed out.';
   static const String cacheError = 'Cache error. Please try again later.';
   static const String noInternetConnection =
       'Please check your internet connection.';
+  static const String connectionError = 'Connection error.';
+  static const String badCertificate = 'Bad certificate.';
 }
