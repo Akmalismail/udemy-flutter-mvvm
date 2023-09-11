836f524f3db106e74b5c010385994353d1ee77c9
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2023-09-11 14:14:04 +0800

    153. Lecture 153 - Add Home ViewModel Inputs and Outputs

--- lib/presentation/main/home/home_view_model.dart
+++ lib/presentation/main/home/home_view_model.dart
@@ -5,7 +5,8 @@ import 'package:udemy_flutter_mvvm/domain/model/model.dart';
 import 'package:udemy_flutter_mvvm/domain/use_case/home_use_case.dart';
 import 'package:udemy_flutter_mvvm/presentation/base/base_view_model.dart';
 
-class HomeViewModel extends BaseViewModel {
+class HomeViewModel extends BaseViewModel
+    with HomeViewModelInputs, HomeViewModelOutputs {
   HomeUseCase _homeUseCase;
 
   final StreamController _serviceStreamController =
@@ -27,8 +28,37 @@ class HomeViewModel extends BaseViewModel {
     _bannerStreamController.close();
     super.dispose();
   }
+
+  @override
+  Sink get inputBanners => _bannerStreamController.sink;
+
+  @override
+  Sink get inputServices => _serviceStreamController.sink;
+
+  @override
+  Sink get inputStores => _storeStreamController.sink;
+
+  @override
+  Stream<List<Banner>> get outputBanners =>
+      _bannerStreamController.stream.map((banner) => banner);
+
+  @override
+  Stream<List<Service>> get outputServices =>
+      _serviceStreamController.stream.map((service) => service);
+
+  @override
+  Stream<List<Store>> get outputStores =>
+      _storeStreamController.stream.map((store) => store);
 }
 
-abstract class HomeViewModelInput {}
+abstract class HomeViewModelInputs {
+  Sink get inputStores;
+  Sink get inputServices;
+  Sink get inputBanners;
+}
 
-abstract class HomeViewModelOutput {}
+abstract class HomeViewModelOutputs {
+  Stream<List<Store>> get outputStores;
+  Stream<List<Service>> get outputServices;
+  Stream<List<Banner>> get outputBanners;
+}
