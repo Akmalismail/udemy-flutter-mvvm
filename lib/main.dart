d0f1c99ba4e5a6f910ec49678d1c1c18a99a8a24
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2023-09-11 20:41:15 +0800

    175. Lecture 175 - Update Main function with Localisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:udemy_flutter_mvvm/app/di.dart';
+import 'package:udemy_flutter_mvvm/presentation/resources/language_manager.dart';
 
 import 'app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await initAppModule();
-  runApp(MyApp());
+  runApp(EasyLocalization(
+    supportedLocales: const [englishLocale, arabicLocale],
+    path: assetsPathLocalisations,
+    child: Phoenix(
+      child: MyApp(),
+    ),
+  ));
 }
