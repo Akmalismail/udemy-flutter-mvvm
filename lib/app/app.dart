7249d8f8bef142cb6af0aa928de859cd83f7ef90
Muhammad Akmal Bin Ismail <muhammadakmal.ismail@tmrnd.com.my>
2023-09-11 20:44:16 +0800

    176. Lecture 176 - Update Application Class and Settings Screen with localisation

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:udemy_flutter_mvvm/app/app_preferences.dart';
+import 'package:udemy_flutter_mvvm/app/di.dart';
 import 'package:udemy_flutter_mvvm/presentation/resources/routes_manager.dart';
 import 'package:udemy_flutter_mvvm/presentation/resources/theme_manager.dart';
 
@@ -23,11 +26,25 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  AppPreferences _appPreferences = instance<AppPreferences>();
+
+  @override
+  void didChangeDependencies() {
+    _appPreferences.getLocale().then((locale) {
+      context.setLocale(locale);
+    });
+
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: MaterialApp(
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
         debugShowCheckedModeBanner: false,
         onGenerateRoute: RouteGenerator.getRoute,
         initialRoute: Routes.splashRoute,
